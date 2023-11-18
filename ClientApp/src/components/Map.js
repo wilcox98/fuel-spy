a7da829da494e3216e65d5484a952ba1a1061f45
wilcox <rongunowilcox@gmail.com>
2023-11-18 12:48:10 +1100

    use custom marker

--- ClientApp/src/components/Map.js
+++ ClientApp/src/components/Map.js
@@ -1,50 +1,54 @@
 /* eslint-disable array-callback-return */
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
-import React from "react";
-import { Icon } from "leaflet";
+import L from "leaflet";
+import React, { useState } from "react";
 import fuel from "./../fuel-station.png";
-// import data from "./location.json";
-import mapData from "./interpreter.json";
 import { PriceModal } from "./PriceModal";
 
 export const MapView = ({
   zoom = 13,
   center = [-1.286389, 36.817223],
   towns,
+  stations,
 }) => {
-  var geoJSONData = [];
-  geoJSONData = Array.from(mapData["elements"]);
-  console.log(geoJSONData);
+  const [_zoom, setZoom] = useState(zoom);
+
+  // console.log(geoJSONData);
   return (
-    <MapContainer center={center} zoom={zoom} scrollWheelZoom={false}>
+    <MapContainer center={center} zoom={_zoom} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* {geoJSONData.forEach((element) => console.log(element))} */}
-      {geoJSONData.map((town, index) => {
-        if (town.type === "node") {
-          return (
-            <Marker
-              key={index}
-              position={[town.lat, town.lon]}
-              icon={
-                new Icon({
-                  iconUrl: fuel,
-                  iconSize: [40, 40],
-                  iconAnchor: [20, 30],
-                })
-              }
-            >
-              <Popup>
-                <PriceModal town={town}></PriceModal>
-              </Popup>
-            </Marker>
-          );
-        } else {
-          //   return <></>;
-        }
+      {stations.map((station, index) => {
+        return (
+          <Marker
+            key={index}
+            position={[station.lat, station.lon]}
+            icon={L.divIcon({
+              className: "custom-popup",
+              iconSize: [40, 40],
+              iconAnchor: [25, 65],
+              html:
+                // ReactDOMServer.renderToString(<SVGIconComponent perc={200} />),
+                `<div class="row">
+                  <div class="col position-relative">
+                    <span class="priceTag position-absolute translate-middle start-50   fs-6 fw-bold">${station.fuelPrice.petrol}</span>
+                  <img height="40px" src="${fuel}" ></img>
+                  </div>
+                </div>`,
+            })}
+            eventHandlers={{
+              click: () => {
+                setZoom(15);
+              },
+            }}
+          >
+            <Popup>
+              <PriceModal station={station}></PriceModal>
+            </Popup>
+          </Marker>
+        );
       })}
     </MapContainer>
   );
