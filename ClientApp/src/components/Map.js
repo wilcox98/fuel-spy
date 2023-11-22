545ab7bc59addef4382fa604f1b724fc4607c309
wilcox <rongunowilcox@gmail.com>
2023-11-22 13:26:23 +1100

    show price banner if available

--- ClientApp/src/components/Map.js
+++ ClientApp/src/components/Map.js
@@ -8,11 +8,12 @@ import { PriceModal } from "./PriceModal";
 export const MapView = ({
   zoom = 13,
   center = [-1.286389, 36.817223],
-  towns,
+  fuelType,
   stations,
 }) => {
   const [_zoom, setZoom] = useState(zoom);
-
+  let priceWidget;
+  let selectedFuelType;
   // console.log(geoJSONData);
   return (
     <MapContainer center={center} zoom={_zoom} scrollWheelZoom={false}>
@@ -21,23 +22,42 @@ export const MapView = ({
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {stations.map((station, index) => {
+        if (station.fuelPrice !== null) {
+          if (fuelType === "Super") {
+            selectedFuelType = station.fuelPrice.petrol;
+          } else if (fuelType === "Diesel") {
+            selectedFuelType = station.fuelPrice.diesel;
+          } else if (fuelType === "Kerosene") {
+            selectedFuelType = station.fuelPrice.kerosene;
+          }
+          priceWidget = L.divIcon({
+            className: "custom-popup",
+            iconSize: [40, 40],
+            iconAnchor: [25, 65],
+            html: `<div class="row">
+            <div class="col position-relative">
+              <span class="priceTag position-absolute translate-middle start-50   fs-6 fw-bold">${selectedFuelType}</span>
+            <img height="40px" src="${fuel}" ></img>
+            </div>
+          </div>`,
+          });
+        } else {
+          priceWidget = L.divIcon({
+            className: "custom-popup",
+            iconSize: [40, 40],
+            iconAnchor: [22, 33],
+            html: `<div class="row">
+            <div class="col position-relative">
+            <img height="40px" src="${fuel}" ></img>
+            </div>
+          </div>`,
+          });
+        }
         return (
           <Marker
             key={index}
             position={[station.lat, station.lon]}
-            icon={L.divIcon({
-              className: "custom-popup",
-              iconSize: [40, 40],
-              iconAnchor: [25, 65],
-              html:
-                // ReactDOMServer.renderToString(<SVGIconComponent perc={200} />),
-                `<div class="row">
-                  <div class="col position-relative">
-                    <span class="priceTag position-absolute translate-middle start-50   fs-6 fw-bold">${station.fuelPrice.petrol}</span>
-                  <img height="40px" src="${fuel}" ></img>
-                  </div>
-                </div>`,
-            })}
+            icon={priceWidget}
             eventHandlers={{
               click: () => {
                 setZoom(15);
